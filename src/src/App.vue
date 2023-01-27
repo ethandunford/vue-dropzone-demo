cd7294227988e7dfe74158dd1178fba9e39765c0
ethan dunford <ethan.dunford@inchora.com>
2023-01-27 10:05:20 +0000

    header added

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,47 +1,46 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="container py-4">
+      <div class="pb-3 mb-4 border-bottom">
+        <a
+          href="/"
+          class="d-flex align-items-center text-dark text-decoration-none"
+        >
+          <svg
+            version="1.1"
+            viewBox="0 0 261.76 226.69"
+            xmlns="http://www.w3.org/2000/svg"
+            height="40px"
+            width="40px"
+          >
+            <g transform="matrix(1.3333 0 0 -1.3333 -76.311 313.34)">
+              <g transform="translate(178.06 235.01)">
+                <path
+                  d="m0 0-22.669-39.264-22.669 39.264h-75.491l98.16-170.02 98.16 170.02z"
+                  fill="#41b883"
+                />
+              </g>
+              <g transform="translate(178.06 235.01)">
+                <path
+                  d="m0 0-22.669-39.264-22.669 39.264h-36.227l58.896-102.01 58.896 102.01z"
+                  fill="#34495e"
+                />
+              </g>
+            </g>
+          </svg>
+          <span class="fs-4">Vue Dropzone Demo </span>
+        </a>
+      </div>
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+<style lang="scss" scoped>
+main {
+  svg {
+    margin-right: 1rem;
   }
 }
 </style>
