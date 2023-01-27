abc26a58cd27fb5da12f22293ee08a249bab1885
ethan dunford <ethan.dunford@inchora.com>
2023-01-27 10:48:47 +0000

    button fix

--- src/src/components/FileHandler.vue
+++ src/src/components/FileHandler.vue
@@ -12,7 +12,7 @@
       <input type="text" class="form-control" placeholder="New file name" />
     </div>
     <div class="col-auto">
-      <button type="submit" class="btn btn-d-anger mb-3">Remove</button>
+      <button type="submit" class="btn btn-danger mb-3">Remove</button>
     </div>
     <div class="col-auto">
       <button type="submit" class="btn btn-primary mb-3">Upload</button>
